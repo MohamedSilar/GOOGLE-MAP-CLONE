6c73f1409b0904283977bbea7fd5758bfb774880
Your Name <you@example.com>
2024-08-07 20:03:03 +0530

    first app

---
+++ app/src/main/java/com/example/mymap/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.mymap;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.internal.ICameraUpdateFactoryDelegate;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //change 1
+
+
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.id_map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+        LatLng location = new LatLng(9.9252, 78.1198);
+        googleMap.addMarker(new MarkerOptions().position(location).title("MADURAI"));
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(location,12));
+
+    }
+}
